797f5e32d5b103965408aa3323f3112b04ef1c3e
Alex Ngassaki <ngassakialex20@gmail.com>
2026-03-11 22:44:42 +0100

    Ajout des animations dans la page Contacts

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,33 +1,72 @@
+import {motion} from "framer-motion";
+import logo from "../assets/logo.png";
+import send from "../assets/send.png";
+import facebook from "../assets/facebook.png";
+import instagram from "../assets/instagram.png";
+import tiktok from "../assets/tiktok.png";
+import whatsapp from "../assets/whatsapp.png";
+
 export const Contacts = () => {
 
     return (
         <div className="p-10">
-            <h1 className="text-xl font-bold">LOGO</h1>
+            <div className="w-14 aspect-square">
+                <img src={logo} alt="Logo" className="object-contain"/>
+            </div>
             <div className="flex mt-20 mb-15">
-                <label>News Lettter</label>
-                <div className="w-fit bg-primary px-5 py-2 rounded-3xl ml-auto font-semibold cursor-pointer text-white">
-                    Contactez Nous
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="email" className="font-semibold">News Lettter</label>
+                    <div className="flex gap-2">
+                        <input
+                            type="email" 
+                            id="email" 
+                            placeholder="bongissa@exemple.com" 
+                            className="px-4 py-2 border-2 border-primary bg-green-200 rounded-2xl"
+                        />
+                        <motion.div 
+                            className="w-10 aspect-square bg-primary rounded-full flex justify-center items-center cursor-pointer"
+                            whileTap={{scale: 0.95}}
+                            whileHover={{scale: 1.05}}
+                        >
+                                <img src={send} alt="..." className="object-contain"/>
+                        </motion.div>
+                    </div>
                 </div>
+            <motion.button 
+                whileTap={{scale: 0.95}}
+                whileHover={{scale: 1.05}}
+                className="w-fit bg-primary px-5 py-2 mt-[10%] rounded-3xl font-semibold cursor-pointer text-white ml-auto"
+            >
+                Contactez Nous
+            </motion.button>
             </div>
             <div className="w-6xl h-0.5 bg-gray-400 mr-auto ml-auto"></div>
             <div className="grid grid-cols-3 mt-15">
                 <div className="flex justify-start gap-4">
-                    <div className="w-12 aspect-square bg-secondary rounded-full mt-auto"></div>
-                    <div className="w-12 aspect-square bg-secondary rounded-full mt-auto"></div>
-                    <div className="w-12 aspect-square bg-secondary rounded-full mt-auto"></div>
-                    <div className="w-12 aspect-square bg-secondary rounded-full mt-auto"></div>
+                    <div className="w-10 aspect-square  rounded-full mt-auto cursor-pointer">
+                        <img src={facebook} alt="..." className="object-contain"/>
+                    </div>
+                    <div className="w-10 aspect-square  rounded-full mt-auto cursor-pointer">
+                        <img src={instagram} alt="..." className="object-contain"/>
+                    </div>
+                    <div className="w-10 aspect-square  rounded-full mt-auto cursor-pointer">
+                        <img src={whatsapp} alt="..." className="object-contain"/>
+                    </div>
+                    <div className="w-10 aspect-square  rounded-full mt-auto cursor-pointer">
+                        <img src={tiktok} alt="..." className="object-contain"/>
+                    </div>
                 </div>
                 <div className="flex flex-col justify-between">
                     <h1 className="text-2xs font-bold">Navigation</h1>
-                    <h2 className="text-2xs">A propos</h2>
-                    <h2 className="text-2xs">Nos projets</h2>
-                    <h2 className="text-2xs">FAQs</h2>
+                    <a href="" className="text-2xs">A propos</a>
+                    <a href=""  className="text-2xs">Nos projets</a>
+                    <a href=""  className="text-2xs">FAQs</a>
                 </div>
                 <div className="flex flex-col justify-between h-30">
                     <h1 className="text-2xs font-bold">Contacts</h1>
-                    <h2 className="text-2xs">(+242) 06 767 66 43</h2>
-                    <h2 className="text-2xs">bongissa@exemple.com</h2>
-                    <h2 className="text-2xs">7 rue eby, Massengo</h2>
+                    <p className="text-2xs">(+242) 06 767 66 43</p>
+                    <p className="text-2xs">bongissa@exemple.com</p>
+                    <p className="text-2xs">7 rue eby, Massengo</p>
                 </div>
             </div>
         </div>
--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -1,25 +1,83 @@
 import { QuestionCard } from "../components/questionCard";
 import { ContactCard } from "../components/contactCard";
 import { ServiceCard } from "../components/serviceCard";
+import { motion } from "framer-motion";
 
+
+const container = {
+    hidden: {opacity: 0},
+    visible: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.2,
+            delayChildren: 0.4
+        }
+    }
+};
+
+const titre = {
+    hidden: {opacity: 0, y: -120},
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {druration: 0.6}
+    }
+}
+
+const slideRight = {
+    hidden: {opacity: 0, x: 100},
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {duration: 0.6}
+    }
+}
+
+const slideBottom = {
+    hidden: {opacity: 0, y: 120},
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {duration: 0.6}
+    }
+}
 
 export const Questions = () => {
 
     return (
-        <div className="min-h-screen p-10">
-            <h1 className="text-3xl font-bold text-center">Questions ?</h1>
+        <motion.div 
+            className="min-h-screen p-10"
+            variants={container}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once: true, amount: 0.4, margin: "150px"}}
+        >
+            <motion.h1 
+                className="text-3xl font-bold text-center"
+                variants={titre}
+            >
+                Questions ?
+            </motion.h1>
             <div className="grid grid-cols-2 mt-[3%] min-h-screen">
                 <div className="flex flex-col gap-5">
-                    <QuestionCard question="Q'est ce qu'est l'ONG Bongissa ?" reponse="L'ONG Bongissa represente..."/>
-                    <QuestionCard question="Q'est ce qu'est l'ONG Bongissa ?" reponse="L'ONG Bongissa represente..."/>
-                    <QuestionCard question="Q'est ce qu'est l'ONG Bongissa ?" reponse="L'ONG Bongissa represente..."/>
-                    <QuestionCard question="Q'est ce qu'est l'ONG Bongissa ?" reponse="L'ONG Bongissa represente..."/>
+                    <motion.div variants={slideBottom} className="">
+                        <QuestionCard question="Q'est ce qu'est l'ONG Bongissa ?" reponse="L'ONG Bongissa represente..."/>
+                    </motion.div>
+                    <motion.div variants={slideBottom} className="">
+                        <QuestionCard question="Q'est ce qu'est l'ONG Bongissa ?" reponse="L'ONG Bongissa represente..."/>
+                    </motion.div>
+                    <motion.div variants={slideBottom} className="">
+                        <QuestionCard question="Q'est ce qu'est l'ONG Bongissa ?" reponse="L'ONG Bongissa represente..."/>
+                    </motion.div>
+                    <motion.div variants={slideBottom} className="">
+                        <QuestionCard question="Q'est ce qu'est l'ONG Bongissa ?" reponse="L'ONG Bongissa represente..."/>
+                    </motion.div>
                 </div>
-                <div className="flex flex-col items-end gap-4">
+                <motion.div variants={slideRight} className="flex flex-col items-end gap-4">
                     <ContactCard/>
                     <ServiceCard/>
-                </div>
+                </motion.div>
             </div>
-        </div>
+        </motion.div>
     )
 }
